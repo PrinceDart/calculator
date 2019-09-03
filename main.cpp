cf26e9b18037440e4cecd0021f70ddb2f09fe4aa
prince kumar <42884671+princedart@users.noreply.github.com>
2019-09-03 10:52:09 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int num1,num2;
+    char op;
+    cout<<"enter first number:";
+    cin>>num1;
+    cout<<"enter operator:";
+    cin>>op;
+    cout<<"enter second number:";
+    cin>>num2;
+
+    int result;
+    if(op =='+'){
+        result = num1 +num2;
+    }
+    else if(op =='-'){
+        result = num1 - num2;
+    }
+    else if(op =='*'){
+        result = num1*num2;
+    }
+     else if(op =='/'){
+        result = num1/num2;
+    }
+    else{
+        cout<<"invalid operator";
+    }
+    cout<<result;
+
+    return 0;
+}
